a9f93dfd9bd168f1b9f52c645e68bcf59a3ffbfb
Sisingh <singh_siddhu@yahoo.com>
2019-06-16 01:00:51 +0800

    added tests

--- main.go
+++ main.go
@@ -193,7 +193,17 @@ func declareExchange(ch *amqp.Channel, exchName string, exchType string) {
 	failOnError(err, "Failed to declare an exchange")
 }
 
+// Reverse returns its argument string reversed rune-wise left to right.
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func main() {
+	Reverse("siddhartha")
 	exchName := "sid_exchange"
 	rk := "sid_rk"
 	queueName := "sid_queue"
